43845cd1565fcc2a72fe989d07b1d3e1aabd875d
anatolyefimov <efimo.tolik@gmail.com>
2019-12-12 22:13:44 +0300

    dasjgkjashgkd

--- video_detection.py
+++ video_detection.py
@@ -5,7 +5,6 @@ import mrcnn.config
 import mrcnn.utils
 from mrcnn.model import MaskRCNN
 from pathlib import Path
-import matplotlib.pyplot as plt
 
 
 # Configuration that will be used by the Mask-RCNN library
@@ -46,7 +45,7 @@ if not os.path.exists(COCO_MODEL_PATH):
 IMAGE_DIR = os.path.join(ROOT_DIR, "images")
 
 # Video file or camera to process - set this to 0 to use your webcam instead of a video file
-IMAGE_SOURCE = "image.png"
+VIDEO_SOURCE = "4.mp4"
 
 # Create a Mask-RCNN model in inference mode
 model = MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=MaskRCNNConfig())
@@ -58,19 +57,24 @@ model.load_weights(COCO_MODEL_PATH, by_name=True)
 parked_car_boxes = None
 
 # Load the video file we want to run detection on
-frame = cv2.imread(IMAGE_SOURCE)
+video_capture = cv2.VideoCapture(VIDEO_SOURCE)
 
+# Loop over each frame of video
+while video_capture.isOpened():
+    success, frame = video_capture.read()
+    print(frame)
+    if not success:
+        break
 
+    # Convert the image from BGR color (which OpenCV uses) to RGB color
+    rgb_image = frame[:, :, ::-1]
 
-# Convert the image from BGR color (which OpenCV uses) to RGB color
-rgb_image = frame[:, :, ::-1]
-
-# Run the image through the Mask R-CNN model to get results.
-results = model.detect([rgb_image], verbose=0)
+    # Run the image through the Mask R-CNN model to get results.
+    results = model.detect([rgb_image], verbose=0)
 
     # Mask R-CNN assumes we are running detection on multiple images.
     # We only passed in one image to detect, so only grab the first result.
-r = results[0]
+    r = results[0]
 
     # The r variable will now have the results of detection:
     # - r['rois'] are the bounding box of each detected object
@@ -79,24 +83,25 @@ r = results[0]
     # - r['masks'] are the object masks for each detected object (which gives you the object outline)
 
     # Filter the results to only grab the car / truck bounding boxes
-car_boxes = get_car_boxes(r['rois'], r['class_ids'])
+    car_boxes = get_car_boxes(r['rois'], r['class_ids'])
 
-print("Cars found in frame of video:")
+    print("Cars found in frame of video:")
 
     # Draw each box on the frame
-for box in car_boxes:
-    print("Car: ", box)
+    for box in car_boxes:
+        print("Car: ", box)
 
-    y1, x1, y2, x2 = box
+        y1, x1, y2, x2 = box
 
         # Draw the box
-    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
 
     # Show the frame of video on the screen
-plt.imshow(frame)
-plt.show()
-if cv2.waitKey(1) & 0xFF == ord('q'):
-    exit()
+    cv2.imshow('Video', frame)
+
+    # Hit 'q' to quit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 # Clean up everything when finished
 # video_capture.release()
